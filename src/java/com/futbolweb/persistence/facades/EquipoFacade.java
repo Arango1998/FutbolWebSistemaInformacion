1b7fbf4f9106e4cc61acd0e9b3ee67292a0f188e
Cristian Suesca <Cristian Suesca@10.1.237.21>
2017-04-19 11:09:33 -0500

    Primer Push

---
+++ src/java/com/futbolweb/persistence/facades/EquipoFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.futbolweb.persistence.facades;
+
+import com.futbolweb.persistence.entities.Equipo;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Cristian Suesca
+ */
+@Stateless
+public class EquipoFacade extends AbstractFacade<Equipo> {
+
+    @PersistenceContext(unitName = "web_futbol_sistema_informacionPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public EquipoFacade() {
+        super(Equipo.class);
+    }
+    
+}
