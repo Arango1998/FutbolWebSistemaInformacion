dde2058df6761045464dbdfa72c275a66af9b332
Cristian Suesca <Cristian Suesca@10.1.237.180>
2017-07-18 18:18:43 -0500

    seguidad

--- src/java/com/futbolweb/login/beans/SeguridadManagedBean.java
+++ src/java/com/futbolweb/login/beans/SeguridadManagedBean.java
@@ -5,7 +5,10 @@
  */
 package com.futbolweb.login.beans;
 
+import com.futbolweb.persistence.entities.Permiso;
+import com.futbolweb.persistence.entities.Rol;
 import com.futbolweb.persistence.entities.Usuario;
+import com.futbolweb.persistence.facades.RolFacade;
 import com.futbolweb.persistence.facades.UsuarioFacade;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -21,38 +24,63 @@ import javax.faces.context.FacesContext;
 @Dependent
 public class SeguridadManagedBean {
 
-      private Usuario usuario;
+      private Usuario us;
     @EJB
     private UsuarioFacade usuarioFacade;
+    
+    @EJB private RolFacade rolF;
+    
     
     public SeguridadManagedBean() {
     }
 
     public Usuario getUsuario() {
-        return usuario;
+        return us;
     }
 
     public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
+        this.us = usuario;
     }
     
      @PostConstruct
     public void init() {
-        usuario = new Usuario();
+        us = new Usuario();
     }
     
     
     
       public void verificarSesion() {
         try {
-            Usuario user = (Usuario) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("user");
-            if (user == null) {
-                FacesContext.getCurrentInstance().getExternalContext().redirect("./../permisos.xhtml");
+            Usuario usuario = (Usuario) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuario");
+            if(usuario == null){
+             FacesContext.getCurrentInstance().getExternalContext().redirect("../../permisos.xhtml?faces-redirect=true");
             }
+            System.out.println("wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww");
+            Usuario user = usuarioFacade.find(Integer.valueOf(usuario.obtenerLlavePrimaria()));
+            System.out.println("uuuuuuuuuuuuuuuuuuu");
+            if (!verificarPermisos(rolF.find(Integer.valueOf(user.getIdTipoRol().obtenerLlavePrimaria())))) {
+                FacesContext.getCurrentInstance().getExternalContext().redirect("../../permisos.xhtml?faces-redirect=true");
+            }
+            
         } catch (Exception e) {
-
         }
-
+    }
+    
+    
+    private boolean verificarPermisos(Rol r) {
+        boolean perm = false;
+        System.out.println("Rol: " + r.getNombreRol());
+        FacesContext fc = FacesContext.getCurrentInstance();
+        String url = "/faces" + fc.getExternalContext().getRequestPathInfo();
+        for (Permiso p : r.getPermisoList()) {
+            if (p.getNombrePermiso() != null && p.getUrl() != null && p.getUrl().equals(url)) {
+                System.out.println(p.getNombrePermiso());
+                perm = true;
+                break;
+            }
+        }
+        
+        return perm;
     }
       
       
